6cd52c127c63f931e81fee10c22532836398c448
adarshranjan97 <adarshranjan97@gmail.com>
2020-01-15 19:03:34 +0530

    new programs

---
+++ circle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define PI 3.14f
+ 
+int main()
+{
+    float rad,area, perm;
+    printf("Enter radius of circle: ");
+    scanf("%f",&rad);
+    area=PI*rad*rad;
+    perm=2*PI*rad;
+    printf("Area of circle: %f \nPerimeter of circle: %f\n",area,perm);
+    return 0;
+}
---
+++ employee.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main()
+{
+    char name[30];
+    float basic,hra,da,pf,gross;
+    printf("Enter name: ");
+    gets(name);
+    printf("Enter Basic Salary: ");
+    scanf("%f",&basic);
+    printf("Enter HRA: ");
+    scanf("%f",&hra);
+    printf("Enter D.A.: ");
+    scanf("%f",&da);
+    pf= (basic*12)/100;
+    gross=basic+da+hra+pf;
+    printf("\nName: %s \nBASIC: %f\nHRA: %f\nDA: %f\nPF: %f\nGROSS SALARY: %f",name,basic,hra,da,pf,gross);
+     
+    return 0;
+}
